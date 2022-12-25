84c91c8719b34e9f6b6063f8acfc799440dd720a
Lola Khudoyberdieva <lola.kh1994@gmail.com>
2022-12-25 20:58:48 +0300

    exercises 9.1-9.7

---
+++ health-calculator/bmiCalculator.ts
@@ -0,0 +1,55 @@
+interface HeightWeight {
+  height: number
+  weight: number
+}
+
+type BmiResult = 'underweight' | 'normal weight' | 'overweight'  | 'obese' | 'none';
+
+const parseArguments1 = (args: Array<string>): HeightWeight => {
+  if(args.length < 4) throw new Error('Not enough arguments');
+  if(args.length > 4) throw new Error('Too many arguments');
+
+  if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    };
+  }
+  else {
+    throw new Error('Provided numbers were not numbers!');
+  }
+};
+
+
+const calculateBmi = (height: number, weight:number): BmiResult => {
+  if (weight === 0 || height === 0)
+    throw new Error('Weight or height cannot be 0');
+  if(isNaN(Number(height)) || isNaN(Number(weight)))
+    throw new Error('Weight or height must be numbers');
+  
+  const o: number = Math.round(weight / Math.pow(height/100, 2) * 100) / 100;
+
+  switch(true) {
+    case (o < 18.5):
+      return 'underweight';
+    case (o >= 18.5 && o < 25):
+      return 'normal weight';
+    case (o >= 25 && o < 30):
+        return 'overweight';
+    case (o >= 30):
+      return 'obese';
+    default:
+      return 'none';
+  }
+};
+
+try {
+  const { height, weight } = parseArguments1(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
---
+++ health-calculator/exerciseCalculator.ts
@@ -0,0 +1,75 @@
+
+interface TargetDailyExercises {
+  exerciseHours: number[]
+  target: number
+}
+
+interface Result {
+  periodLength: number
+  trainingDays: number
+  success: boolean
+  rating: number
+  ratingDescription: string
+  target: number
+  average: number
+}
+
+const parseArguments2 = (args: Array<string>): TargetDailyExercises => {
+  if(args.length < 4) throw new Error('Not enough arguments');
+  let i = 3;
+  const allExercises = [];
+
+  while(!isNaN(Number(args[i]))) {
+    allExercises.push(Number(args[i]));
+    i += 1;
+  }
+
+  if(!isNaN(Number(args[2]))) {
+    return {
+      exerciseHours: [...allExercises],
+      target: Number(args[2])
+    };
+  }
+  else {
+    throw new Error('Provided numbers were not numbers!');
+  }
+};
+
+
+const calculateExercises = (exerciseHours: number[], target: number): Result => {
+  const periodLength = exerciseHours.length;
+  const trainingDays = exerciseHours.filter(e => e !== 0).length;
+ 
+  const totalExerciseHours = exerciseHours.reduce((sum, e) => sum + e, 0);
+  
+  const average = totalExerciseHours / periodLength;
+  const success = average > target;
+
+  const proportion = average / target;
+ 
+  const rating = proportion > 1 ? 3 : proportion < 1 && proportion > 0.75 ? 2 : 1;
+  const ratingDescription = rating === 3 ? 'excellent' : rating === 2 ? 'average' : 'bad';
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
+  };
+
+};
+
+
+try {
+  const { exerciseHours, target} = parseArguments2(process.argv);
+  console.log(calculateExercises(exerciseHours, target));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
